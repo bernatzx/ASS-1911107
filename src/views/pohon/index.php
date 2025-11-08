2aeb7d03e2393a8210985aee28b14c25186460fa
bernatzx <andikasukardi1@gmail.com>
2025-11-08 19:25:39 +0700

    init halaman data pohon

--- src/views/pohon/index.php
+++ src/views/pohon/index.php
@@ -1,3 +1,7 @@
 <?php include_once __DIR__ . "/../../layout/header.php"; ?>
-pohon
+<div class="font-bold mb-[30px]">
+  <span>Data Pohon</span>
+  <br>
+  <span class="text-gray-400 text-sm">Pohon pohon yang telah didata</span>
+</div>
 <?php include_once __DIR__ . "/../../layout/footer.php"; ?>
