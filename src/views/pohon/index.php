8cd828ab4315832366ecbe522500329d8ee5ceea
bernatzx <andikasukardi1@gmail.com>
2025-11-08 19:59:12 +0700

    halaman data pohon v1/fix

--- src/views/pohon/index.php
+++ src/views/pohon/index.php
@@ -1,7 +1,64 @@
 <?php include_once __DIR__ . "/../../layout/header.php"; ?>
-<div class="font-bold mb-[30px]">
-  <span>Data Pohon</span>
-  <br>
-  <span class="text-gray-400 text-sm">Pohon pohon yang telah didata</span>
+<div class="mb-[30px] flex items-center justify-between">
+  <div class="font-bold">
+    <span>Data Pohon</span>
+    <br>
+    <span class="text-gray-400 text-sm">Pohon pohon yang telah didata</span>
+  </div>
+  <div class="text-xs bg-blue-500 py-2 px-3 rounded-md shadow-md text-gray-200 cursor-pointer hover:opacity-70">
+    <i class="fas fa-add"></i>
+    Tambah Data
+  </div>
+</div>
+<div class="border-2 rounded-md">
+  <table class="w-full">
+    <thead class="bg-gray-50 text-xs text-gray-700 uppercase border-b">
+      <tr>
+        <th class="tracking-wider text-left p-3">No.</th>
+        <th class="tracking-wider text-left p-3">Pohon</th>
+        <th class="tracking-wider text-left p-3">Jenis</th>
+        <th class="tracking-wider text-left p-3">Lokasi</th>
+        <th class="tracking-wider text-left p-3 w-20">Desa</th>
+        <th class="tracking-wider text-left p-3 w-20">Kota</th>
+        <th class="tracking-wider text-center p-3">
+          <i class="fas fa-gear"></i>
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr class="odd:bg-white even:bg-gray-50">
+        <td class="tracking-wider text-left p-3">1</td>
+        <td class="tracking-wider text-left p-3">Rhizophora apiculata</td>
+        <td class="tracking-wider text-left p-3">Bakau Minyak</td>
+        <td class="tracking-wider text-left p-3">Kel. Maftutu</td>
+        <td class="tracking-wider text-left p-3">Maftutu</td>
+        <td class="tracking-wider text-left p-3">Tidore</td>
+        <td class="tracking-wider justify-center p-3 flex gap-2">
+          <div class="border border-yellow-600 py-1 px-2 text-yellow-600 rounded-md cursor-pointer hover:bg-gray-200">
+            <i class="fas fa-pencil"></i>
+          </div>
+          <div class="border border-red-600 py-1 px-2 text-red-600 rounded-md cursor-pointer hover:bg-gray-200">
+            <i class="fas fa-trash"></i>
+          </div>
+        </td>
+      </tr>
+      <tr class="odd:bg-white even:bg-gray-50">
+        <td class="tracking-wider text-left p-3">1</td>
+        <td class="tracking-wider text-left p-3">Rhizophora apiculata</td>
+        <td class="tracking-wider text-left p-3">Bakau Minyak</td>
+        <td class="tracking-wider text-left p-3">Kel. Maftutu</td>
+        <td class="tracking-wider text-left p-3">Maftutu</td>
+        <td class="tracking-wider text-left p-3">Tidore</td>
+        <td class="tracking-wider justify-center p-3 flex gap-2">
+          <div class="border border-yellow-600 py-1 px-2 text-yellow-600 rounded-md cursor-pointer hover:bg-gray-200">
+            <i class="fas fa-pencil"></i>
+          </div>
+          <div class="border border-red-600 py-1 px-2 text-red-600 rounded-md cursor-pointer hover:bg-gray-200">
+            <i class="fas fa-trash"></i>
+          </div>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </div>
 <?php include_once __DIR__ . "/../../layout/footer.php"; ?>
