7a2638738bfe5d0bb6b5f61a2e3005e936d1c928
bernatzx <andikasukardi1@gmail.com>
2025-11-09 11:34:50 +0700

    init login display first

---
+++ src/views/auth/login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login Admin | Sistem Informasi Geografis Pemetaan Pohon Mangrove</title>
+</head>
+
+<body>
+  <h1>LOGIN</h1>
+</body>
+
+</html>
