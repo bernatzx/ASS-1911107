7909587f28f357f632aa8dd6bfdd3d4a072ad3fc
bernatzx <andikasukardi1@gmail.com>
2025-11-09 11:29:55 +0700

    tampilan tambah dan edit untuk pohon dan jenis

---
+++ src/components/logo.php
@@ -0,0 +1,16 @@
+<div class="flex grover">
+  <div class="mt-[4px]">
+    <img style="height: 62px;" src="<?= base("/src/assets/img/logo.png") ?>" alt="logo">
+  </div>
+  <div class="text-7xl">
+    M
+  </div>
+  <div class="text-2xl">
+    <div class="mt-[12px] mb-[-8px]">
+      angrovee
+    </div>
+    <div>
+      aluku Utara
+    </div>
+  </div>
+</div>
